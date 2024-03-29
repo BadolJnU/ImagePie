c0aa8d603294deb48223bf94790d7170aae543f5
unknown <kbadol135@gmail.com>
2024-03-29 03:39:23 -0400

    title and subtitle of header dynamically updated

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
+import React from "react";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
   return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+    <Header title={transformation.type} subtitle={transformation.subTitle} />
+  );
+};
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
